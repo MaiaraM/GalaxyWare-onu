2dd51b90216035df434048bb0dee71314f26485c
Maiara Martins <maiaramartins093@gmail.com>
2018-10-09 12:27:32 -0300

    final

---
+++ app/src/main/java/com/example/vmac/WatBot/FinalActivity.java
@@ -0,0 +1,13 @@
+package com.example.vmac.WatBot;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class FinalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.final_activity);
+    }
+}
